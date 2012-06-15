f3e69ef3ff54e7c117b217deb8acf5df1fce6aa6
Claudio Perez Gamayo <crossblaim@gmail.com>
2012-06-15 19:46:26 +0200

    Remove redgreen dependence
    That gem does not work with ruby 1.9 and latest TestUnit supports
    coloration out of the box

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,6 @@ require 'stringio'
 require 'test/unit'
 
 require 'rubygems'
-require 'redgreen' rescue nil
 
 require 'shoulda'
 require 'mocha'
@@ -12,46 +11,46 @@ require File.dirname(__FILE__) + '/../lib/simplepay'
 
 module Simplepay #:nodoc:
   module Macros #:nodoc: all
-    
+
     def should_not_have_service_field(*names)
       klass = model_class
-      
+
       names.each do |name|
         should "not have a field named #{name.inspect}" do
           assert !klass.fields.any? { |f| f.name == name }
         end
       end
     end
-    
+
     def should_have_service_field(*names)
       delegate_class, service_name, required = get_options!(names, :class, :as, :required)
       klass = model_class
-      
+
       names.each do |name|
         should "have a field named #{name.inspect}" do
           message = "#{klass} does not have a Field named #{name.inspect}"
           assert klass.fields.any? { |f| f.name == name }, message
         end
-        
+
         context "#{name} field" do
           should("delegate to #{delegate_class}") do
             message = "#{klass} field #{name.inspect} failed to delegate to #{delegate_class}"
             field   = klass.fields.detect { |f| f.name == name }
             assert_equal delegate_class, field.delegate, message
           end if delegate_class
-          
+
           should("not delegate") do
             field   = klass.fields.detect { |f| f.name == name }
             message = "#{klass} field #{name.inspect} unexpectedly delegated to #{field.delegate}"
             assert !field.delegated?, message
           end unless delegate_class
-          
+
           should("have a #{service_name} service name") do
             message = "#{klass} field #{name.inspect} failed to have expected service name"
             field   = klass.fields.detect { |f| f.name == name }
             assert_equal service_name, field.service_name
           end if service_name
-          
+
           should("#{"not " unless required}be required") do
             message = "#{klass} field #{name.inspect} was#{" not" if required} required"
             field   = klass.fields.detect { |f| f.name == name }
@@ -60,11 +59,11 @@ module Simplepay #:nodoc:
         end
       end
     end unless method_defined?(:should_have_service_field)
-    
-    
+
+
     private
-    
-    
+
+
     # Returns the values for the entries in the args hash who's keys are listed in the wanted array.
     # Will raise if there are keys in the args hash that aren't listed.
     def get_options!(args, *wanted)
@@ -74,7 +73,7 @@ module Simplepay #:nodoc:
       raise ArgumentError, "Unsupported options given: #{opts.keys.join(', ')}" unless opts.keys.empty?
       return *ret
     end unless defined?(:get_options!)
-    
+
   end
 end
 
